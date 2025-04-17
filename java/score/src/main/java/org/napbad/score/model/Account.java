dc6e767d466dcbc6b8321588c5e586c0641b874d
Napbad <napbad.sen@gmail.com>
2025-04-18 07:46:13 +0800

    tmp

--- java/score/src/main/java/org/napbad/score/model/Account.java
+++
@@ -1,9 +0,0 @@
-package org.napbad.score.model;
-
-import lombok.Data;
-
-@Data
-public abstract class Account {
-    private int id;
-    private String passwd;
-}
--- java/score/src/main/java/org/napbad/score/model/SortType.java
+++
@@ -1,5 +0,0 @@
-package org.napbad.score.model;
-
-public enum SortType {
-    BY_TOTAL_GRADE, BY_STUDENT_ID
-}
